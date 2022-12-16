fcdc966ed91e42b809b8e2a1f8d905b53d2ba111
kkkl37 <112578026+kkkl37@users.noreply.github.com>
2022-12-16 07:25:31 +0000

    rename

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 import pandas as pd
-import numpy as np
+from sklearn.preprocessing import StandardScaler
 import warnings
 warnings.filterwarnings("ignore")
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import coo_matrix, hstack
+from sklearn.metrics.pairwise import linear_kernel
+import numpy as np
 import streamlit as st
 
 data = pd.read_csv("./clean_df/clean_df.csv")
@@ -12,18 +16,51 @@ text_data = ['title','director','actor','overview','genres_list','key','country'
 data[text_data] = data[text_data].astype(str)
 data["key"] = data["key"].str.encode('ascii', 'ignore').str.decode('ascii')
 
+def to_dummy(col,num = None):
+    li = set()
+    for i in range(len(data[col])):
+        if num is None:
+            try:
+                num = len(data[col][i].split(','))
+            except:
+                print(data[col][i])
+        for act in data[col][i].split(',')[:num]:
+            li.add(act)
+    li = list(li)
+    for element in li:
+        data[element] = data[col].astype(str).str.contains(element, case=False).astype(int)
 
-with open('cosine_sim.npy', 'rb') as f:
-    cosine_sim = np.load(f)
+@st.cache
+def convert(dum):
+    for d in dum:
+        if d == 'actor':
+            to_dummy(d,num = 4)
+        else:
+            to_dummy(d)
+        print(d)
+dum = ['country','director','actor','genres_list','key']
+convert(dum)
 
+X = data.drop(['id','title','director','actor', 'overview','genres_list','key','country'], axis=1)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+tfidf = TfidfVectorizer(stop_words = 'english')  # initialising the TF-IDF Vector object
+tfidf_matrix = tfidf.fit_transform(data['overview'])  # Constructing the TF-IDF Matrix (no. of movies x every word in vocabulary)
+X = hstack([X,tfidf_matrix]).toarray()
+cosine_sim = linear_kernel(X, X)  # Constructing the Cosine Similarity Matrix (no. of movies x no. of movies)
 indices = pd.Series(data.index, index = data['title']).drop_duplicates()
 
-def get_recommendations(title, cosine_sim = cosine_sim,num=10):
+# Function that inputs movie titles and outputs top 10 movies similar to it
+
+def get_recommendations(title, cosine_sim = cosine_sim):
     idx = indices[title]
+
     sim_scores = list(enumerate(cosine_sim[idx]))  # Get the similarity scores of all movies wrt input movie
     sim_scores = sorted(sim_scores, key = lambda x : x[1], reverse = True)
-    sim_scores = sim_scores[1:num+1]
+    sim_scores = sim_scores[1:11]
+
     movie_indices = [i[0] for i in sim_scores]
+
     return data['title'].iloc[movie_indices]
 
 
--- cosine_application.py
+++ cosine_application.py
@@ -1,11 +1,7 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
-from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.sparse import coo_matrix, hstack
-from sklearn.metrics.pairwise import linear_kernel
-import numpy as np
 import streamlit as st
 
 data = pd.read_csv("./clean_df/clean_df.csv")
@@ -16,51 +12,18 @@ text_data = ['title','director','actor','overview','genres_list','key','country'
 data[text_data] = data[text_data].astype(str)
 data["key"] = data["key"].str.encode('ascii', 'ignore').str.decode('ascii')
 
-def to_dummy(col,num = None):
-    li = set()
-    for i in range(len(data[col])):
-        if num is None:
-            try:
-                num = len(data[col][i].split(','))
-            except:
-                print(data[col][i])
-        for act in data[col][i].split(',')[:num]:
-            li.add(act)
-    li = list(li)
-    for element in li:
-        data[element] = data[col].astype(str).str.contains(element, case=False).astype(int)
 
-@st.cache
-def convert(dum):
-    for d in dum:
-        if d == 'actor':
-            to_dummy(d,num = 4)
-        else:
-            to_dummy(d)
-        print(d)
-dum = ['country','director','actor','genres_list','key']
-convert(dum)
+with open('cosine_sim.npy', 'rb') as f:
+    cosine_sim = np.load(f)
 
-X = data.drop(['id','title','director','actor', 'overview','genres_list','key','country'], axis=1)
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
-tfidf = TfidfVectorizer(stop_words = 'english')  # initialising the TF-IDF Vector object
-tfidf_matrix = tfidf.fit_transform(data['overview'])  # Constructing the TF-IDF Matrix (no. of movies x every word in vocabulary)
-X = hstack([X,tfidf_matrix]).toarray()
-cosine_sim = linear_kernel(X, X)  # Constructing the Cosine Similarity Matrix (no. of movies x no. of movies)
 indices = pd.Series(data.index, index = data['title']).drop_duplicates()
 
-# Function that inputs movie titles and outputs top 10 movies similar to it
-
-def get_recommendations(title, cosine_sim = cosine_sim):
+def get_recommendations(title, cosine_sim = cosine_sim,num=10):
     idx = indices[title]
-
     sim_scores = list(enumerate(cosine_sim[idx]))  # Get the similarity scores of all movies wrt input movie
     sim_scores = sorted(sim_scores, key = lambda x : x[1], reverse = True)
-    sim_scores = sim_scores[1:11]
-
+    sim_scores = sim_scores[1:num+1]
     movie_indices = [i[0] for i in sim_scores]
-
     return data['title'].iloc[movie_indices]
 
 
